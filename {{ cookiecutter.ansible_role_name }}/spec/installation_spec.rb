b30fbe6b5ebb082ab43165c74e95004f3285e1b4
Alexandre Chaussier <a.chaussier@infopen.pro>
2016-01-07 16:44:51 +0100

    Change serverspec default file

---
+++ {{ cookiecutter.ansible_role_name }}/spec/installation_spec.rb
@@ -0,0 +1,20 @@
+require 'serverspec'
+
+describe '{{ cookiecutter.ansible_role_name }} Ansible role' do
+
+if ['debian', 'ubuntu'].include?(os[:family])
+    describe 'Specific Debian and Ubuntu family checks' do
+
+        it 'install role packages' do
+            packages = Array[ 'apt' ]
+
+            packages.each do |pkg_name|
+                expect(package(pkg_name)).to be_installed
+            end
+        end
+
+    end
+end
+
+end
+
