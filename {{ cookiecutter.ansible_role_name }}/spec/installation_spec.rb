d49a33b8d3cfc94cbc6da043c8d9ac14bf22b168
Alexandre Chaussier <alexandre.chaussier@tememlio.com>
2016-01-22 14:28:38 +0100

    Remove RSpec backend warning

--- {{ cookiecutter.ansible_role_name }}/spec/installation_spec.rb
+++ {{ cookiecutter.ansible_role_name }}/spec/installation_spec.rb
@@ -1,5 +1,9 @@
 require 'serverspec'
 
+if ENV['TRAVIS']
+    set :backend, :exec
+end
+
 describe '{{ cookiecutter.ansible_role_name }} Ansible role' do
 
 if ['debian', 'ubuntu'].include?(os[:family])
