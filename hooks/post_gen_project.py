614ba5a0a415bd32212b692eec56806b46383537
Alexandre Chaussier <alexandre.chaussier@temelio.com>
2017-05-30 18:51:43 +0200

    Avoid error if symlink already exists

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,8 +1,19 @@
+"""
+Tasks must be done after project generate
+"""
+
 import os
 import sys
 
+
+SOURCE_PATH = 'tests/playbook.yml'
+TARGET_PATH = 'playbook.yml'
+
 try:
-    os.symlink('tests/playbook.yml', 'playbook.yml')
-except:
-    print 'Symlinks create errors !'
+    if os.path.exists(TARGET_PATH):
+        os.unlink(TARGET_PATH)
+
+    os.symlink(SOURCE_PATH, TARGET_PATH)
+except OSError:
+    print('Symlink create error: {}'.format(TARGET_PATH))
     sys.exit(1)
